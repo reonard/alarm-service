d990d2e8011b2383e7639b3d6c8790cae823c677
reonard <reonard@126.com>
2018-11-08 22:26:21 +0800

    add monitor handle

--- processor/monitor_data.go
+++ processor/monitor_data.go
@@ -1,12 +1,13 @@
 package processor
 
 type MonitorData struct {
-	DeviceId         int64                    `json:"ID"`
-	DeviceStatus     int8                     `json:"status"`
-	TimeStamp        string                   `json:"once"`
-	Data             []map[string]interface{} `json:"ProbeData"`
-	AlarmHandle      string                   `json:"AlarmHandle"`
-	AlarmDescription []string                 `json:"AlarmDesc"`
+	DeviceId          int64                    `json:"ID"`
+	DeviceStatus      int8                     `json:"status"`
+	TimeStamp         string                   `json:"once"`
+	Data              []map[string]interface{} `json:"ProbeData"`
+	AlarmHandleStatus int8                     `json:"AlarmHandleStatus"`
+	AlarmHandle       string                   `json:"AlarmHandle"`
+	AlarmDescription  []string                 `json:"AlarmDesc"`
 }
 
 func Save() {
